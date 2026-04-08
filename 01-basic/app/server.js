63d82e74a7e2fa7cf8b303c0d82c296f5f51275c
Yurii Dmytriev <yuramijs@gmail.com>
2026-04-08 14:01:25 +0200

    new one

---
+++ 01-basic/app/server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const os = require('os');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(
+    JSON.stringify(
+      {
+        message: 'Hello from Node.js on ArgoCD!',
+        hostname: os.hostname(),
+        path: req.url,
+        method: req.method,
+        timestamp: new Date().toISOString(),
+      },
+      null,
+      2
+    )
+  );
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on :${PORT}`);
+});
